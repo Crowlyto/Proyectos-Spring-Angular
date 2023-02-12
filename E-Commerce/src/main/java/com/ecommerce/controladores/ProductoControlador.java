98274bc184bd8825e99e3fb96f94069e516bebe7
Crowlyto <leandro.n.nunez@gmail.com>
2023-02-12 15:04:47 -0300

    Metodo create de ProductosControlador

--- E-Commerce/src/main/java/com/ecommerce/controladores/ProductoControlador.java
+++ E-Commerce/src/main/java/com/ecommerce/controladores/ProductoControlador.java
@@ -4,8 +4,11 @@
  */
 package com.ecommerce.controladores;
 
+import com.ecommerce.entidades.Producto;
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,9 +19,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/productos")
 public class ProductoControlador {
     
+    private final Logger LOGGER=LoggerFactory.getLogger(ProductoControlador.class);
+    
     @GetMapping("")
     public String show(){
         return "productos/show";
+    }
+    
+    @GetMapping("/create")
+    public String create(){
+        return "productos/create";
+    }
+    @PostMapping("/save")
+    public String save(Producto producto){
+        LOGGER.info("Este es el objeto producto{}", producto);
+        
+        return "redirect:/productos";
     }
     
 }
