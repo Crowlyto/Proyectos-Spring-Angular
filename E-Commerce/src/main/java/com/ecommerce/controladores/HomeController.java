3811f2cf44e8e920173a488e1c1302df9bf5a0aa
Crowlyto <leandro.n.nunez@gmail.com>
2023-02-16 10:03:00 -0300

    Vista home, vista producto en controlador

--- E-Commerce/src/main/java/com/ecommerce/controladores/HomeController.java
+++ E-Commerce/src/main/java/com/ecommerce/controladores/HomeController.java
@@ -4,11 +4,14 @@
  */
 package com.ecommerce.controladores;
 
+import com.ecommerce.entidades.Producto;
 import com.ecommerce.servicios.ProductoServicio;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -28,4 +31,13 @@ public class HomeController {
         return "usuario/home";
     }
     
+    @GetMapping("/productohome/{id}")
+    public String productoHome(@PathVariable Integer id, Model model){
+        Producto producto=new Producto();
+        Optional<Producto>respuesta= servP.get(id);
+        producto=respuesta.get();
+        
+        model.addAttribute("producto", producto);
+        return "usuario/productohome";
+    }
 }
