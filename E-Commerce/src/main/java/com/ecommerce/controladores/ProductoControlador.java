373feb76ad5336592adb942f8197034e6f4600b6
Crowlyto <leandro.n.nunez@gmail.com>
2023-02-16 00:25:41 -0300

    Implementacion para guardar/editar imagen del producto

--- E-Commerce/src/main/java/com/ecommerce/controladores/ProductoControlador.java
+++ E-Commerce/src/main/java/com/ecommerce/controladores/ProductoControlador.java
@@ -7,6 +7,8 @@ package com.ecommerce.controladores;
 import com.ecommerce.entidades.Producto;
 import com.ecommerce.entidades.Usuario;
 import com.ecommerce.servicios.ProductoServicio;
+import com.ecommerce.servicios.UploadFileServicio;
+import java.io.IOException;
 import java.util.Optional;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -29,6 +33,9 @@ public class ProductoControlador {
     @Autowired
     private ProductoServicio servP;
 
+    @Autowired
+    private UploadFileServicio upload;
+
     @GetMapping("")
     public String show(Model model) {
         model.addAttribute("productos", servP.findAll());
@@ -41,10 +48,25 @@ public class ProductoControlador {
     }
 
     @PostMapping("/save")
-    public String save(Producto producto) {
-        LOGGER.info("Este es el objeto producto{}", producto);
+    public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+
         Usuario user = new Usuario(1, "", "", "", "", "", "");
         producto.setUsuario(user);
+        //imagen
+        if (producto.getId() == null) {//cuando se crea un producto
+            String nombreImagen = upload.saveImage(file);
+            producto.setImagen(nombreImagen);
+        } else {
+            if (file.isEmpty()) {//cuando se edita el producto pero no cambiamos la imagen
+                Producto p = new Producto();
+                p = servP.get(producto.getId()).get();
+                producto.setImagen(p.getImagen());
+
+            } else {
+                  String nombreImagen = upload.saveImage(file);
+            producto.setImagen(nombreImagen);
+            }
+        }
         servP.save(producto);
         return "redirect:/productos";
     }
@@ -56,17 +78,18 @@ public class ProductoControlador {
         Optional<Producto> respuesta = servP.get(id);
         producto = respuesta.get();
         System.out.println(producto.getNombre());
-        model.addAttribute("producto",producto);
+        model.addAttribute("producto", producto);
         return "productos/edit";
     }
+
     @PostMapping("/update")
-    public String update(Producto producto){
+    public String update(Producto producto) {
         servP.update(producto);
         return "redirect:/productos";
     }
-    
+
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Integer id){
+    public String delete(@PathVariable Integer id) {
         servP.delete(id);
         return "redirect:/productos";
     }
